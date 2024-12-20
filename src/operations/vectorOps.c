788fe7ee934805b04e3234996307fd2d0deac3f2
Cooper <chtarbuck@gmail.com>
2024-12-19 22:48:00 -0500

    Began initial setup

---
+++ src/operations/vectorOps.c
@@ -0,0 +1,21 @@
+//Need the custom vector types
+#include "../structs/types.h"
+
+//Adds two physics vectors and returns the sum
+PhysVec add(PhysVec a, PhysVec b) {
+    PhysVec out;
+    out.x = a.x + b.x;
+    out.y = a.y + b.y;
+    out.z = a.z + b.z;
+
+    return out;
+}
+
+//Subtracts two physics vectors and returns the value
+PhysVec subtract(PhysVec a, PhysVec b) {
+    out.x = a.x - b.x;
+    out.y = a.y - b.y;
+    out.z = a.z - b.z;
+
+    return out;
+}
